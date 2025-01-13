9c2ee472586db3bd7139e8eeda80969aee260228
Farhan Y. <yahyafarhan48@gmail.com>
2025-01-13 14:33:56 +0000

    feat: changes to the status bar (#13)

--- src/managers/StatusBarManager.ts
+++ src/managers/StatusBarManager.ts
@@ -2,7 +2,6 @@ import * as vscode from "vscode";
 
 export class StatusBarManager implements vscode.Disposable {
   status!: vscode.StatusBarItem;
-  runWorkflows!: vscode.StatusBarItem;
   constructor() {
     this.status = vscode.window.createStatusBarItem(
       vscode.StatusBarAlignment.Left,
@@ -10,24 +9,7 @@ export class StatusBarManager implements vscode.Disposable {
     );
     this.status.show();
     this.setStatusInactive();
-
-    this.runWorkflows = vscode.window.createStatusBarItem(
-      vscode.StatusBarAlignment.Left,
-      1
-    );
-    this.runWorkflows.command = "openfn.run-workflows";
-  }
-
-  setRunWorkflowsActive() {
-    this.runWorkflows.show();
-    this.runWorkflows.text = "$(debug-start) Run Workflow(s)";
-    this.runWorkflows.tooltip = "Execute a workflow in this workspace";
-    this.runWorkflows.backgroundColor = new vscode.ThemeColor(
-      "statusBarItem.background"
-    );
-  }
-  setRunWorkflowsInactive() {
-    this.runWorkflows.hide();
+    this.status.command = "openfn.run-workflows";
   }
 
   setStatusActive() {
@@ -47,12 +29,11 @@ export class StatusBarManager implements vscode.Disposable {
   }
 
   setStatusAdaptor(adaptor: string) {
-    this.status.text = `OpenFn: ${adaptor}`;
+    this.status.text = `${adaptor}`;
     this.status.tooltip = "OpenFn Workspace detected";
   }
 
   dispose() {
     if (this.status) this.status.dispose();
-    if (this.runWorkflows) this.runWorkflows.dispose();
   }
 }
