1ce97012ee916d9c4706eb688c15ee007661e9c0
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-02-15 23:56:30 +0000

    feat: smart handling type exports

--- src/tsSupport/jsLibs.ts
+++ src/tsSupport/jsLibs.ts
@@ -24,14 +24,19 @@ export function loadLibrary(adaptors: string[], libs: string[]) {
       `/tmp/openfn/repo/node_modules/@openfn/language-${adaptor.refined}/types`
     )
   );
+
+  const globalExports: Record<string, boolean> = {};
+  const namespaces: Record<string, string> = {};
   let activeSource: string = "";
   return function (name: string) {
+    let isIndex = false;
     if (libs.includes(name)) activeSource = TYPESCRIPT_LIB_SOURCE;
     else if (adaptors.includes(name)) activeSource = adaptorsMap.get(name);
     let libPath, content;
 
     if (adaptors.includes(name)) {
       libPath = activeSource + "/index.d.ts";
+      isIndex = true;
     } else if (/^node_modules\//.test(name)) {
       // FIXME: these are mostly libs that are referenced in the lib we want to load. currently not loading well.
       // we load from the local node_modules in this project. it rather want to load from where vscode keeps all installed extensions on disk
@@ -40,15 +45,35 @@ export function loadLibrary(adaptors: string[], libs: string[]) {
     if (!libPath) return "";
     try {
       content = readFileSync(libPath).toString();
-      if (/^Adaptor\.d\.ts$/i.test(name)) {
-        // get adaptor utils. Assuming it'll always be Utils.d.ts
-        const utilsPath = activeSource + "/Utils.d.ts";
-        let utilsContent = "";
-        if (existsSync(utilsPath)) {
-          utilsContent = readFileSync(utilsPath).toString();
-          utilsContent = `export namespace utils { ${utilsContent} }`;
+      // if isIndex parse the code
+      if (isIndex) {
+        // parse the code here
+        const globalMatches = content.matchAll(
+          /export\s+\*\s+from\s+\"(.+)\"/g
+        );
+        const namespacedMatches = content.matchAll(
+          /export\s+\*\s+as\s+(\w+)\s+from\s+\"(.+)\"/g
+        );
+
+        // track all global exports
+        for (const gm of globalMatches) {
+          const importFile = gm[1].replace("./", "") + ".d.ts";
+          if (gm[1]) globalExports[importFile] = true;
         }
-        content = `export {}; declare global { ${utilsContent} ${content} }`;
+
+        // track all namespace exports
+        for (const nm of namespacedMatches) {
+          const nspace = nm[1];
+          const importFile = nm[2].replace("./", "") + ".d.ts";
+          if (nspace && importFile) namespaces[importFile] = nspace;
+        }
+      }
+
+      // do which ever fits
+      if (globalExports[name]) {
+        content = `export {}; declare global { ${content} }`;
+      } else if (namespaces[name]) {
+        content = `export {}; declare global { export namespace ${namespaces[name]} { ${content} } }`;
       }
     } catch (e) {
       // `console`.log(`Unable to load library ${name} at ${libPath}`);
