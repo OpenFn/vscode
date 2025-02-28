13ea24c96058173f7808d8a2e65add4c95058958
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-02-28 14:18:17 +0000

    fix: input filename cleanup

--- src/utils/cleanupFilename.ts
+++ src/utils/cleanupFilename.ts
@@ -1,6 +1,7 @@
 export default function cleanupFilename(name: string) {
   return name
     .trim()
+    .replace(/\s+/g, "-")
     .replace(/[^a-zA-Z0-9.\-_]+/g, "-")
     .replace(/-+/g, "-")
     .replace(/^-+|-+$/g, "")
